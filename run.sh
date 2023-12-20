1fc0267d2e5b5437104c5827a81f54c774081d60
jdey4 <jdey4@jhmi.edu>
2023-12-20 16:57:20 -0500

    run bash file

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ do
     for shift in {1..6..1}
     do
         echo "Doing slot $slot shift $shift"
-        python3 ./main.py --experiment split_cifar100_sc_5 --approach ewc_coscl --lamb 40000 --lamb1 0.02 --use_TG --s_gate 100 --seed 0 --slot slot --shift shift
+        python3 ./main.py --experiment split_cifar100_sc_5 --approach ewc_coscl --lamb 40000 --lamb1 0.02 --use_TG --s_gate 100 --seed 0 --slot $slot --shift $shift
     done
 done
