47f771cdf0a68e7844d361d538f05bc59a5c4466
jdey4 <jdey4@jhmi.edu>
2023-12-21 10:56:56 -0500

    run bash file

--- approaches/ewc_coscl.py
+++ approaches/ewc_coscl.py
@@ -244,6 +244,6 @@ class Appr(object):
         loss_reg=0
         if t>0:
             for name, param in self.model.named_parameters():
-                loss_reg+=torch.sum(self.fisher[name]*(self.old_param[name] - param).pow(2))/2
+                loss_reg+=0#torch.sum(self.fisher[name]*(self.old_param[name] - param).pow(2))/2
         return self.ce(output,targets) + self.lamb * loss_reg
 
